19cd491a274c87a9be989480baec6f68aa35620e
JamesBirchall-dev <jamesbdorel@gmail.com>
2025-12-01 12:48:14 +0000

    database and depkoymnent settings updated

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q5@1u&dzer*m1nh1ik4&wzf&^_93@2%38-bb-mj537j-ma3*4n'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -83,7 +83,6 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
--- env.py
+++ env.py
@@ -1,3 +1,4 @@
 import os
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_fDAQoBtc19uy@ep-calm-king-aghtgcbv.c-2.eu-central-1.aws.neon.tech/fiber_decor_romp_152640")
+os.environ.setdefault("SECRET_KEY", "71E74988B9D344A4")
